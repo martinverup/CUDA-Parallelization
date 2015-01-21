a9ff2f37f53892fb572255c1bbf44e94fa264a72
Martin Verup <Martin@Martins-MacBook-Pro.local>
2015-01-21 19:26:18 +0100

    More functions

--- matmult.cu
+++ matmult.cu
@@ -1,251 +1,374 @@
-#include <cblas.h>
 #include <cuda_runtime.h>
 #include <cublas_v2.h>
+#include <stdio.h>
+#include <helper_cuda.h>
 
-#define BS 256
+extern "C" {
+#include <cblas.h>
 
-void matmult_nat(int m, int n, int k, double *A, double *B, double *C)
-{
-    int i, j, h;
-    for (i = 0; i < m; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            C[n * i + j] = 0.0;
-            for (h = 0; h < k; h++)
-            {
-                C[n * i + j] += A[k * i + h] * B[n * h + j];
-            }
-        }
-    }
-}
+#define BLOCK_SIZE 32
 
-void matmult_mnk(int m, int n, int k, double *A, double *B, double *C)
-{
-    int i, j, h;
-    for (i = 0; i < m * n; i++)
+    void matmult_nat(int m, int n, int k, double *A, double *B, double *C)
     {
-        C[i] = 0.0;
-    }
-    for (i = 0; i < m; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            for (h = 0; h < k; h++)
-            {
-                C[n * i + j] += A[k * i + h] * B[n * h + j];
-            }
-        }
-    }
-}
-
-void matmult_nmk(int m, int n, int k, double *A, double *B, double *C)
-{
-    int i, j, h;
-    for (i = 0; i < m * n; i++)
-    {
-        C[i] = 0.0;
-    }
-    for (j = 0; j < n; j++)
-    {
-        for (i = 0; i < m; i++)
-        {
-            for (h = 0; h < k; h++)
-            {
-                C[n * i + j] += A[k * i + h] * B[n * h + j];
-            }
-        }
-    }
-}
-
-void matmult_nkm(int m, int n, int k, double *A, double *B, double *C)
-{
-    int i, j, h;
-    for (i = 0; i < m * n; i++)
-    {
-        C[i] = 0.0;
-    }
-    for (j = 0; j < n; j++)
-    {
-        for (h = 0; h < k; h++)
-        {
-            for (i = 0; i < m; i++)
-            {
-                C[n * i + j] += A[k * i + h] * B[n * h + j];
-            }
-        }
-    }
-}
-
-void matmult_kmn(int m, int n, int k, double *A, double *B, double *C)
-{
-    int i, j, h;
-    for (i = 0; i < m * n; i++)
-    {
-        C[i] = 0.0;
-    }
-    for (h = 0; h < k; h++)
-    {
+        int i, j, h;
         for (i = 0; i < m; i++)
         {
             for (j = 0; j < n; j++)
             {
-                C[n * i + j] += A[k * i + h] * B[n * h + j];
+                C[n * i + j] = 0.0;
+                for (h = 0; h < k; h++)
+                {
+                    C[n * i + j] += A[k * i + h] * B[n * h + j];
+                }
             }
         }
     }
-}
 
-void matmult_knm(int m, int n, int k, double *A, double *B, double *C)
-{
-    int i, j, h;
-    for (i = 0; i < m * n; i++)
+    void matmult_mnk(int m, int n, int k, double *A, double *B, double *C)
     {
-        C[i] = 0.0;
+        int i, j, h;
+        for (i = 0; i < m * n; i++)
+        {
+            C[i] = 0.0;
+        }
+        for (i = 0; i < m; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                for (h = 0; h < k; h++)
+                {
+                    C[n * i + j] += A[k * i + h] * B[n * h + j];
+                }
+            }
+        }
     }
-    for (h = 0; h < k; h++)
+
+    void matmult_nmk(int m, int n, int k, double *A, double *B, double *C)
     {
+        int i, j, h;
+        for (i = 0; i < m * n; i++)
+        {
+            C[i] = 0.0;
+        }
         for (j = 0; j < n; j++)
         {
             for (i = 0; i < m; i++)
             {
-                C[n * i + j] += A[k * i + h] * B[n * h + j];
+                for (h = 0; h < k; h++)
+                {
+                    C[n * i + j] += A[k * i + h] * B[n * h + j];
+                }
             }
         }
     }
-}
 
-void matmult_mkn(int m, int n, int k, double *A, double *B, double *C)
-{
-    int i, j, h;
-    for (i = 0; i < m * n; i++)
+    void matmult_nkm(int m, int n, int k, double *A, double *B, double *C)
     {
-        C[i] = 0.0;
+        int i, j, h;
+        for (i = 0; i < m * n; i++)
+        {
+            C[i] = 0.0;
+        }
+        for (j = 0; j < n; j++)
+        {
+            for (h = 0; h < k; h++)
+            {
+                for (i = 0; i < m; i++)
+                {
+                    C[n * i + j] += A[k * i + h] * B[n * h + j];
+                }
+            }
+        }
     }
-    for (i = 0; i < m; i++)
+
+    void matmult_kmn(int m, int n, int k, double *A, double *B, double *C)
     {
+        int i, j, h;
+        for (i = 0; i < m * n; i++)
+        {
+            C[i] = 0.0;
+        }
+        for (h = 0; h < k; h++)
+        {
+            for (i = 0; i < m; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    C[n * i + j] += A[k * i + h] * B[n * h + j];
+                }
+            }
+        }
+    }
+
+    void matmult_knm(int m, int n, int k, double *A, double *B, double *C)
+    {
+        int i, j, h;
+        for (i = 0; i < m * n; i++)
+        {
+            C[i] = 0.0;
+        }
         for (h = 0; h < k; h++)
         {
             for (j = 0; j < n; j++)
             {
-                C[n * i + j] += A[k * i + h] * B[n * h + j];
+                for (i = 0; i < m; i++)
+                {
+                    C[n * i + j] += A[k * i + h] * B[n * h + j];
+                }
             }
         }
     }
-}
 
-void matmult_lib(int m, int n, int k, double *A, double *B, double *C)
-{
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k, 1.0, A, k, B, n, 0.0, C, n);
-}
-
-__global__ void matmult_gpu1_thread(int m, int n, int k, double *A, double *B, double *C)
-{
-    int row = blockIdx.y * blockDim.y + threadIdx.y;
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
-    if (row <= m && col <= n)
+    void matmult_mkn(int m, int n, int k, double *A, double *B, double *C)
     {
+        int i, j, h;
+        for (i = 0; i < m * n; i++)
+        {
+            C[i] = 0.0;
+        }
+        for (i = 0; i < m; i++)
+        {
+            for (h = 0; h < k; h++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    C[n * i + j] += A[k * i + h] * B[n * h + j];
+                }
+            }
+        }
+    }
+
+    void matmult_lib(int m, int n, int k, double *A, double *B, double *C)
+    {
+        cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k, 1.0, A, k, B, n, 0.0, C, n);
+    }
+
+    __global__ void matmult_gpu1_kernel(int m, int n, int k, double *A, double *B, double *C)
+    {
+        int col = threadIdx.x + blockIdx.x * blockDim.x;
+        int row = threadIdx.y + blockIdx.y * blockDim.y;
+        if (row < m && col < n)
+        {
+            double sum = 0.0;
+            int i;
+            for (i = 0; i < k; i++)
+            {
+                sum += A[k * row + i] * B[n * i + col];
+            }
+            C[n * row + col] = sum;
+        }
+    }
+
+    void matmult_gpu1(int m, int n, int k, double *A, double *B, double *C)
+    {
+        double *device_a, *device_b, *device_c;
+        checkCudaErrors(cudaMalloc((void **) &device_a, m * k * sizeof(double)));
+        checkCudaErrors(cudaMalloc((void **) &device_b, k * n * sizeof(double)));
+        checkCudaErrors(cudaMalloc((void **) &device_c, m * n * sizeof(double)));
+
+        checkCudaErrors(cudaMemcpy(device_a, A, m * k * sizeof(double), cudaMemcpyHostToDevice));
+        checkCudaErrors(cudaMemcpy(device_b, B, k * n * sizeof(double), cudaMemcpyHostToDevice));
+
+        dim3 DimGrid((n + BLOCK_SIZE - 1) / BLOCK_SIZE, (m + BLOCK_SIZE - 1) / BLOCK_SIZE);
+        dim3 DimBlock(BLOCK_SIZE, BLOCK_SIZE);
+
+        matmult_gpu1_kernel <<< DimGrid, DimBlock >>> (m, n, k, device_a, device_b, device_c);
+
+        checkCudaErrors(cudaDeviceSynchronize());
+
+        checkCudaErrors(cudaMemcpy(C, device_c, m * n * sizeof(double), cudaMemcpyDeviceToHost));
+
+        checkCudaErrors(cudaFree(device_a));
+        checkCudaErrors(cudaFree(device_b));
+        checkCudaErrors(cudaFree(device_c));
+    }
+
+    __global__ void matmult_gpu2_kernel(int m, int n, int k, double *A, double *B, double *C)
+    {
+        int col = threadIdx.x + blockIdx.x * blockDim.x;
+        int row = threadIdx.y + blockIdx.y * blockDim.y * 2;
+
         double sum = 0.0;
-        int h;
-        for (h = 0; h < k; h++)
+        int i;
+        if (row < m && col < n)
         {
-            sum += A[k * row + h] * B[n * h + col];
+            for (i = 0; i < k; i++)
+            {
+                sum += A[k * row + i] * B[n * i + col];
+            }
+            C[n * row + col] = sum;
         }
-        C[n * row + col] = sum;
-    }
-}
-
-void matmult_gpu1(int m, int n, int k, double *A, double *B, double *C)
-{
-    double *device_a, *device_b, *device_c;
-    cudaMalloc((void **) &device_a, m * k * sizeof(double));
-    cudaMalloc((void **) &device_b, k * n * sizeof(double));
-    cudaMalloc((void **) &device_c, m * n * sizeof(double));
-
-    cudaMemcpy(device_a, A, m * k * sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(device_b, B, k * n * sizeof(double), cudaMemcpyHostToDevice);
-
-    dim3 DimGrid((n + BS - 1) / BS, (m + BS - 1) / BS);
-    dim3 DimBlock(BS, BS);
-
-    matmult_gpu1_thread <<< DimGrid, DimBlock >>> (m, n, k, device_a, device_b, device_c);
-
-    cudaDeviceSynchronize();
-
-    cudaMemcpy(C, device_c, m * n * sizeof(double), cudaMemcpyDeviceToHost);
-
-    cudaFree(device_a);
-    cudaFree(device_b);
-    cudaFree(device_c);
-}
-
-__global__ void matmult_gpu2_thread(int m, int n, int k, double *A, double *B, double *C)
-{
-    __shared__ double A_smem[32][32];
-    int row = blockIdx.y * blockDim.y + threadIdx.y;
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
-    double sum = 0.0;
-    int h;
-    for (h = 0; h < n; h++)
-    {
-        if (row < m)
+        row += 1;
+        if (row < m && col < n)
         {
-            A_smem[h][threadIdx.x] = A[row * n + h];
-        }
-        __syncthreads();
-        sum += A_smem[h][threadIdx.x] * B[n * h + col];
+	        sum = 0.0;
+	        for (i = 0; i < k; i++)
+	        {
+	        	sum += A[k * row + i] * B[n * i + col];
+	        }
+	        C[n * row + col] = sum;
+	    }
     }
 
-    if (row < m)
+    void matmult_gpu2(int m, int n, int k, double *A, double *B, double *C)
     {
-        C[n * row + col] = sum;
+        double *device_a, *device_b, *device_c;
+        checkCudaErrors(cudaMalloc((void **) &device_a, m * k * sizeof(double)));
+        checkCudaErrors(cudaMalloc((void **) &device_b, k * n * sizeof(double)));
+        checkCudaErrors(cudaMalloc((void **) &device_c, m * n * sizeof(double)));
+
+        checkCudaErrors(cudaMemcpy(device_a, A, m * k * sizeof(double), cudaMemcpyHostToDevice));
+        checkCudaErrors(cudaMemcpy(device_b, B, k * n * sizeof(double), cudaMemcpyHostToDevice));
+
+        dim3 DimGrid((n / 2 + BLOCK_SIZE - 1) / BLOCK_SIZE, (m / 2 + BLOCK_SIZE - 1) / BLOCK_SIZE);
+        dim3 DimBlock(BLOCK_SIZE, BLOCK_SIZE);
+
+        matmult_gpu2_kernel <<< DimGrid, DimBlock >>> (m, n, k, device_a, device_b, device_c);
+
+        checkCudaErrors(cudaDeviceSynchronize());
+        checkCudaErrors(cudaGetLastError());
+
+        checkCudaErrors(cudaMemcpy(C, device_c, m * n * sizeof(double), cudaMemcpyDeviceToHost));
+
+        checkCudaErrors(cudaFree(device_a));
+        checkCudaErrors(cudaFree(device_b));
+        checkCudaErrors(cudaFree(device_c));
     }
-}
 
-void matmult_gpu2(int m, int n, int k, double *A, double *B, double *C)
-{
-    double *device_a, *device_b, *device_c;
-    cudaMalloc((void **) &device_a, m * k * sizeof(double));
-    cudaMalloc((void **) &device_b, k * n * sizeof(double));
-    cudaMalloc((void **) &device_c, m * n * sizeof(double));
+    __global__ void matmult_gpu3_kernel(int m, int n, int k, double *A, double *B, double *C)
+    {
 
-    cudaMemcpy(device_a, A, m * k * sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(device_b, B, k * n * sizeof(double), cudaMemcpyHostToDevice);
+    }
 
-    dim3 DimGrid((n + BS - 1) / BS, (m + BS - 1) / BS);
-    dim3 DimBlock(BS, BS);
+    void matmult_gpu3(int m, int n, int k, double *A, double *B, double *C)
+    {
+        double *device_a, *device_b, *device_c;
+        checkCudaErrors(cudaMalloc((void **) &device_a, m * k * sizeof(double)));
+        checkCudaErrors(cudaMalloc((void **) &device_b, k * n * sizeof(double)));
+        checkCudaErrors(cudaMalloc((void **) &device_c, m * n * sizeof(double)));
 
-    matmult_gpu2_thread <<< DimGrid, DimBlock >>> (m, n, k, device_a, device_b, device_c);
+        checkCudaErrors(cudaMemcpy(device_a, A, m * k * sizeof(double), cudaMemcpyHostToDevice));
+        checkCudaErrors(cudaMemcpy(device_b, B, k * n * sizeof(double), cudaMemcpyHostToDevice));
 
-    cudaDeviceSynchronize();
+        dim3 DimGrid((n / 2 + BLOCK_SIZE - 1) / BLOCK_SIZE, (m / 2 + BLOCK_SIZE - 1) / BLOCK_SIZE);
+        dim3 DimBlock(BLOCK_SIZE, BLOCK_SIZE);
 
-    cudaMemcpy(C, device_c, m * n * sizeof(double), cudaMemcpyDeviceToHost);
+        matmult_gpu3_kernel <<< DimGrid, DimBlock >>> (m, n, k, device_a, device_b, device_c);
 
-    cudaFree(device_a);
-    cudaFree(device_b);
-    cudaFree(device_c);
-}
+        checkCudaErrors(cudaDeviceSynchronize());
+        checkCudaErrors(cudaGetLastError());
 
-void matmult_gpu3(int m, int n, int k, double *A, double *B, double *C)
-{
+        checkCudaErrors(cudaMemcpy(C, device_c, m * n * sizeof(double), cudaMemcpyDeviceToHost));
 
-}
+        checkCudaErrors(cudaFree(device_a));
+        checkCudaErrors(cudaFree(device_b));
+        checkCudaErrors(cudaFree(device_c));
+    }
 
-void matmult_gpu4(int m, int n, int k, double *A, double *B, double *C)
-{
+    __global__ void matmult_gpu4_kernel(int m, int n, int k, double *A, double *B, double *C)
+    {
+    	int col = threadIdx.x + blockIdx.x * blockDim.x;
+        int row = threadIdx.y + blockIdx.y * blockDim.y;
 
-}
+        __shared__ double A_s[BLOCK_SIZE][BLOCK_SIZE + 1];
 
-void matmult_gpu5(int m, int n, int k, double *A, double *B, double *C)
-{
+        if (row < m && col < n) {
+	        double sum = 0.0;
+		     for (int i = 0; i < k; i += BLOCK_SIZE) {
+		        A_s[threadIdx.y][threadIdx.x] = A[i * BLOCK_SIZE * blockIdx.y + i];//A[blockIdx.y * k + i + blockIdx.x];
+		        __syncthreads();
+		        for (int j = 0; j < BLOCK_SIZE; j++) {
+		           sum += A_s[threadIdx.y][j] * B[n * j + col];
+		        }
+		        C[n * row + col] = sum;
+		     }
+    	}
+    }
 
-}
+    void matmult_gpu4(int m, int n, int k, double *A, double *B, double *C)
+    {
+        double *device_a, *device_b, *device_c;
+        checkCudaErrors(cudaMalloc((void **) &device_a, m * k * sizeof(double)));
+        checkCudaErrors(cudaMalloc((void **) &device_b, k * n * sizeof(double)));
+        checkCudaErrors(cudaMalloc((void **) &device_c, m * n * sizeof(double)));
 
-void matmult_gpulib(int m, int n, int k, const double *A, const double *B, double *C)
-{
-    cublasHandle_t handle;
-    cublasCreate(&handle);
-    const double alpha = 1.0, beta = 0.0;
-    cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, &alpha, A, k, B, n, &beta, C, n);
-    cublasDestroy(handle);
+        checkCudaErrors(cudaMemcpy(device_a, A, m * k * sizeof(double), cudaMemcpyHostToDevice));
+        checkCudaErrors(cudaMemcpy(device_b, B, k * n * sizeof(double), cudaMemcpyHostToDevice));
+
+        dim3 DimGrid((n / 2 + BLOCK_SIZE - 1) / BLOCK_SIZE, (m / 2 + BLOCK_SIZE - 1) / BLOCK_SIZE);
+        dim3 DimBlock(BLOCK_SIZE, BLOCK_SIZE);
+
+        matmult_gpu4_kernel <<< DimGrid, DimBlock >>> (m, n, k, device_a, device_b, device_c);
+
+        checkCudaErrors(cudaDeviceSynchronize());
+
+        checkCudaErrors(cudaMemcpy(C, device_c, m * n * sizeof(double), cudaMemcpyDeviceToHost));
+
+        checkCudaErrors(cudaFree(device_a));
+        checkCudaErrors(cudaFree(device_b));
+        checkCudaErrors(cudaFree(device_c));
+    }
+
+    __global__ void matmult_gpu5_kernel(int m, int n, int k, double *A, double *B, double *C)
+    {
+
+    }
+
+    void matmult_gpu5(int m, int n, int k, double *A, double *B, double *C)
+    {
+        double *device_a, *device_b, *device_c;
+        checkCudaErrors(cudaMalloc((void **) &device_a, m * k * sizeof(double)));
+        checkCudaErrors(cudaMalloc((void **) &device_b, k * n * sizeof(double)));
+        checkCudaErrors(cudaMalloc((void **) &device_c, m * n * sizeof(double)));
+
+        checkCudaErrors(cudaMemcpy(device_a, A, m * k * sizeof(double), cudaMemcpyHostToDevice));
+        checkCudaErrors(cudaMemcpy(device_b, B, k * n * sizeof(double), cudaMemcpyHostToDevice));
+
+        dim3 DimGrid((n / 2 + BLOCK_SIZE - 1) / BLOCK_SIZE, (m / 2 + BLOCK_SIZE - 1) / BLOCK_SIZE);
+        dim3 DimBlock(BLOCK_SIZE, BLOCK_SIZE);
+
+        matmult_gpu5_kernel <<< DimGrid, DimBlock >>> (m, n, k, device_a, device_b, device_c);
+
+        checkCudaErrors(cudaDeviceSynchronize());
+
+        checkCudaErrors(cudaMemcpy(C, device_c, m * n * sizeof(double), cudaMemcpyDeviceToHost));
+
+        checkCudaErrors(cudaFree(device_a));
+        checkCudaErrors(cudaFree(device_b));
+        checkCudaErrors(cudaFree(device_c));
+    }
+
+    void matmult_gpulib(int m, int n, int k, double *A, double *B, double *C)
+    {
+
+        double *device_a, *device_b, *device_c, *trans_C;
+        checkCudaErrors(cudaMalloc((void **) &device_a, m * k * sizeof(double)));
+        checkCudaErrors(cudaMalloc((void **) &device_b, k * n * sizeof(double)));
+        checkCudaErrors(cudaMalloc((void **) &device_c, m * n * sizeof(double)));
+        trans_C = (double *) malloc(m * n * sizeof(double));
+
+        checkCudaErrors(cudaMemcpy(device_a, A, m * k * sizeof(double), cudaMemcpyHostToDevice));
+        checkCudaErrors(cudaMemcpy(device_b, B, k * n * sizeof(double), cudaMemcpyHostToDevice));
+
+        cublasHandle_t handle;
+        cublasCreate(&handle);
+        const double alpha = 1.0, beta = 0.0;
+        cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_T, m, n, k, &alpha, device_a, k, device_b, n, &beta, device_c, m);
+        cublasDestroy(handle);
+
+        checkCudaErrors(cudaMemcpy(trans_C, device_c, m * n * sizeof(double), cudaMemcpyDeviceToHost));
+
+        int row, col;
+        for (row = 0; row < m; row++)
+        {
+            for (col = 0; col < n; col++)
+            {
+                C[n * row + col] = trans_C[m * col + row];
+            }
+        }
+
+        checkCudaErrors(cudaFree(device_a));
+        checkCudaErrors(cudaFree(device_b));
+        checkCudaErrors(cudaFree(device_c));
+    }
 }
