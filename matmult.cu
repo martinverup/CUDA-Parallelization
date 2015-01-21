ac154f464edea7e2534508e83edc528fce220111
Martin Verup <Martin@Martins-MacBook-Pro.local>
2015-01-21 21:59:28 +0100

    Cleaned up your mess

--- matmult.cu
+++ matmult.cu
@@ -8,136 +8,6 @@ extern "C" {
 
 #define BLOCK_SIZE 16
 
-    void matmult_nat(int m, int n, int k, double *A, double *B, double *C)
-    {
-        int i, j, h;
-        for (i = 0; i < m; i++)
-        {
-            for (j = 0; j < n; j++)
-            {
-                C[n * i + j] = 0.0;
-                for (h = 0; h < k; h++)
-                {
-                    C[n * i + j] += A[k * i + h] * B[n * h + j];
-                }
-            }
-        }
-    }
-
-    void matmult_mnk(int m, int n, int k, double *A, double *B, double *C)
-    {
-        int i, j, h;
-        for (i = 0; i < m * n; i++)
-        {
-            C[i] = 0.0;
-        }
-        for (i = 0; i < m; i++)
-        {
-            for (j = 0; j < n; j++)
-            {
-                for (h = 0; h < k; h++)
-                {
-                    C[n * i + j] += A[k * i + h] * B[n * h + j];
-                }
-            }
-        }
-    }
-
-    void matmult_nmk(int m, int n, int k, double *A, double *B, double *C)
-    {
-        int i, j, h;
-        for (i = 0; i < m * n; i++)
-        {
-            C[i] = 0.0;
-        }
-        for (j = 0; j < n; j++)
-        {
-            for (i = 0; i < m; i++)
-            {
-                for (h = 0; h < k; h++)
-                {
-                    C[n * i + j] += A[k * i + h] * B[n * h + j];
-                }
-            }
-        }
-    }
-
-    void matmult_nkm(int m, int n, int k, double *A, double *B, double *C)
-    {
-        int i, j, h;
-        for (i = 0; i < m * n; i++)
-        {
-            C[i] = 0.0;
-        }
-        for (j = 0; j < n; j++)
-        {
-            for (h = 0; h < k; h++)
-            {
-                for (i = 0; i < m; i++)
-                {
-                    C[n * i + j] += A[k * i + h] * B[n * h + j];
-                }
-            }
-        }
-    }
-
-    void matmult_kmn(int m, int n, int k, double *A, double *B, double *C)
-    {
-        int i, j, h;
-        for (i = 0; i < m * n; i++)
-        {
-            C[i] = 0.0;
-        }
-        for (h = 0; h < k; h++)
-        {
-            for (i = 0; i < m; i++)
-            {
-                for (j = 0; j < n; j++)
-                {
-                    C[n * i + j] += A[k * i + h] * B[n * h + j];
-                }
-            }
-        }
-    }
-
-    void matmult_knm(int m, int n, int k, double *A, double *B, double *C)
-    {
-        int i, j, h;
-        for (i = 0; i < m * n; i++)
-        {
-            C[i] = 0.0;
-        }
-        for (h = 0; h < k; h++)
-        {
-            for (j = 0; j < n; j++)
-            {
-                for (i = 0; i < m; i++)
-                {
-                    C[n * i + j] += A[k * i + h] * B[n * h + j];
-                }
-            }
-        }
-    }
-
-    void matmult_mkn(int m, int n, int k, double *A, double *B, double *C)
-    {
-        int i, j, h;
-        for (i = 0; i < m * n; i++)
-        {
-            C[i] = 0.0;
-        }
-        for (i = 0; i < m; i++)
-        {
-            for (h = 0; h < k; h++)
-            {
-                for (j = 0; j < n; j++)
-                {
-                    C[n * i + j] += A[k * i + h] * B[n * h + j];
-                }
-            }
-        }
-    }
-
     void matmult_lib(int m, int n, int k, double *A, double *B, double *C)
     {
         cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k, 1.0, A, k, B, n, 0.0, C, n);
@@ -310,36 +180,36 @@ extern "C" {
 
     __global__ void matmult_gpu4_kernel(int m, int n, int k, double *A, double *B, double *C)
     {
-	__shared__ double A_sub[BLOCK_SIZE][BLOCK_SIZE];
-	//different sizes block indencis
-	int blockX = blockIdx.x;
-	int blockY = blockIdx.y;
-	int threadX = threadIdx.x;
-	int threadY = threadIdx.y;
-	int row = blockY * BLOCK_SIZE + threadY;
-	int col = blockX * BLOCK_SIZE + threadX;
-	double sum = 0.0;
-	
-	int i,j;
-	for(i = 0; i < k; i += BLOCK_SIZE) {
-		//calculating the A & B blocks
-		if (row < m && (i + threadX) < k)
-		{
-			A_sub[threadY][threadX] = A[(row * k) + i + threadX];
-		} else {
-			A_sub[threadY][threadX] = 0.0;
-		}
-		__syncthreads();
+        __shared__ double A_s[BLOCK_SIZE][BLOCK_SIZE];
 
-		//summing the blocks after they are calculated
-		for(j = 0; j < BLOCK_SIZE; ++j) {
-			sum += A_sub[threadY][j] * B[(j+i) * n + col];
-		}
-		__syncthreads();
-	}
-	if(row < m && col < n) {
-		C[row * n + col] = sum;	
-	}
+        int row = blockIdx.y * BLOCK_SIZE + threadIdx.y;
+        int col = blockIdx.x * BLOCK_SIZE + threadIdx.x;
+
+        double sum = 0.0;
+
+        int i, j;
+        for (i = 0; i < k; i += BLOCK_SIZE)
+        {
+            if (row < m && (i + threadIdx.x) < k)
+            {
+                A_s[threadIdx.y][threadIdx.x] = A[(k * row) + i + threadIdx.x];
+            }
+            else
+            {
+                A_s[threadIdx.y][threadIdx.x] = 0.0;
+            }
+            __syncthreads();
+
+            for (j = 0; j < BLOCK_SIZE; ++j)
+            {
+                sum += A_s[threadIdx.y][j] * B[(j + i) * n + col];
+            }
+            __syncthreads();
+        }
+        if (row < m && col < n)
+        {
+            C[row * n + col] = sum;
+        }
     }
 
     void matmult_gpu4(int m, int n, int k, double *A, double *B, double *C)
